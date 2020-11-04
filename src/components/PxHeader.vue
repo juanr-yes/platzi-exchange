9a853db5c90fb10680349e631a213282c02d0d9f
jreyes_admsys <jreyes@admsys.cl>
2020-11-03 23:11:15 -0300

    Commit inicial

---
+++ src/components/PxHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="shadow w-screen">
+    <nav>
+      <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <router-link :to="{name: 'home'}" style="display: inherit">
+            <px-icon/>
+            <span class="font-semibold text-xl tracking-tight ml-2" style="align-self: center;">
+              PlatziExchange
+            </span>
+          </router-link>
+        </div>
+        <div class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto">
+          <div class="text-sm lg:flex-grow"></div>
+        </div>
+      </nav>
+    </nav>
+  </header>
+</template>
+
+<script>
+  import PxIcon from "@/components/PxIcon";
+  export default {
+    components: {
+      PxIcon
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/CoinDetail.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="flex-col">
+    <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'"/>
+    </div>
+    <template v-if="asset.id && !isLoading">
+      <div class="flex flex-col sm:flex-row justify-around items-center">
+        <div class="flex flex-col items-center">
+          <img class="w-20 h-20 mr-5" :src="`https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`" :alt="asset.name"/>
+          <h1 class="text-5xl">
+            {{ asset.name }}
+            <small class="sm:mr-2 text-gray-500">{{ asset.symbol }}</small>
+          </h1>
+        </div>
+        <div class="my-10 flex flex-col">
+          <ul>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Ranking</b>
+              <span>#{{ asset.rank }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio actual</b>
+              <span>{{ asset.priceUsd | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio más bajo</b>
+              <span>{{ min | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio más alto</b>
+              <span>{{ max | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Precio Promedio</b>
+              <span>{{ avg | dollar }}</span>
+            </li>
+            <li class="flex justify-between">
+              <b class="text-gray-600 mr-10 uppercase">Variación 24hs</b>
+              <span>{{ asset.changePercent24Hr | percent }}</span>
+            </li>
+          </ul>
+        </div>
+        <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
+          <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Cambiar
+          </button>
+          <div class="flex flex-row my-5">
+            <label class="w-full" for="convertValue">
+              <input id="convertValue" type="number" class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"/>
+            </label>
+          </div>
+          <span class="text-xl"></span>
+        </div>
+      </div>
+      <line-chart :colors="['orange']" :min="min" :max="max" :data="history.map(h => [h.date, parseFloat(h.priceUsd).toFixed(2)])" class="my-10"/>
+    </template>
+  </div>
+</template>
+
+<script>
+  import api from "@/api"
+  export default {
+    name: 'CoinDetail',
+    data() {
+      return {
+        isLoading: false,
+        asset: {},
+        history: []
+      }
+    },
+    created() {
+      this.getCoin();
+    },
+    computed: {
+      min() {
+        return Math.min(
+          ... this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
+        )
+      },
+      max() {
+        return Math.max(
+          ... this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
+        )
+      },
+      avg() {
+        return Math.abs(
+          ... this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
+        )
+      }
+    },
+    methods: {
+      getCoin() {
+        this.isLoading = true;
+        const id = this.$route.params.id
+        Promise.all([
+          api.getAsset(id),
+          api.getAssetHistory(id),
+        ]).then(([asset, history]) => {
+          this.asset = asset,
+          this.history = history
+        })
+        .finally(() => this.isLoading = false)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
